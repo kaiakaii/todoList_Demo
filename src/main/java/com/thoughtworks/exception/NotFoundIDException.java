697f6de48c0acbaf688d7c7c7306c9b096dd5e5f
SUNSK <senkk512@163.com>
2020-08-06 20:12:20 +0800

    [Todo][Sky] refactor: refactor global exception

--- src/main/java/com/thoughtworks/exception/NotFoundIDException.java
+++ src/main/java/com/thoughtworks/exception/NotFoundIDException.java
@@ -1,8 +1,9 @@
 package com.thoughtworks.springbootemployee.exception;
 
+
 import com.thoughtworks.exception.ExceptionMessage;
 
-public class NotFoundIDException extends Throwable {
+public class NotFoundIDException extends Exception {
     public NotFoundIDException(ExceptionMessage notFoundId) {
     }
 }
